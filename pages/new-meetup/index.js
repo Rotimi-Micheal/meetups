3791cd4b597b854b6013b688d60d19beec95d052
Rotimi-Micheal <mikelater8@gmail.com>
2022-12-13 00:28:43 +0100

    commit

---
+++ pages/new-meetup/index.js
@@ -0,0 +1,38 @@
+// our-domain.com/new-meetup
+import Head from "next/head";
+
+import { useRouter } from "next/router";
+import { Fragment } from "react";
+import NewMeetupForm from "../../components/meetups/NewMeetupForm";
+
+const NewMeetupPage = () => {
+  const router = useRouter();
+  const addMeetupHandler = async (enteredMeetupData) => {
+    const response = await fetch("/api/new-meetup", {
+      method: "POST",
+      body: JSON.stringify(enteredMeetupData),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    const data = await response.json();
+    console.log(data);
+
+    router.push("/");
+  };
+
+  return (
+    <Fragment>
+      <Head>
+        <title>Add a New Meetup</title>
+        <meta
+          name="description"
+          content="Add your own meetups creat amazing opportunities"
+        />
+      </Head>
+      <NewMeetupForm onAddMeetup={addMeetupHandler} />
+    </Fragment>
+  );
+};
+
+export default NewMeetupPage;
